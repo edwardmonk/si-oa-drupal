990615f126a564ee15459a138b00f29fc6363da0
Edward Monk <edwardmonk@users.noreply.github.com>
2023-02-16 10:32:05 -0500

    Initial Commit

--- src/Form/OpenAccessApiConnectionForm.php
+++ src/Form/OpenAccessApiConnectionForm.php
@@ -71,16 +71,39 @@ class OpenAccessApiConnectionForm extends ConfigFormBase {
   }
 
  /**
- *    * Submits the search form.
- *       */
- public function submitForm(array &$form, FormStateInterface $form_state) {
-    $config = $this->configFactory->getEditable('smithsonian_open_access.open_access_api_connection');
-    $config->set('api_base_url', $form_state->getValue('api_base_url'));
-    $config->set('api_key', $form_state->getValue('api_key'));
-    $config->save();
+ * {@inheritdoc}
+ */
+public function submitForm(array &$form, FormStateInterface $form_state) {
+  // Save the API base URL and API key to the module's configuration.
+  $config = $this->config('smithsonian_open_access.settings');
+  $config->set('api_base_url', $form_state->getValue('api_base_url'));
+  $config->set('api_key', $form_state->getValue('api_key'));
+  $config->save();
 
-    parent::submitForm($form, $form_state);
+  // If a search term was entered, submit the search form and display the
+  // response in a status message.
+  $search_term = $form_state->getValue('search');
+  if (!empty($search_term)) {
+    $api_endpoint = $config->get('api_base_url') . '/search';
+
+    // Build the API request URL.
+    $api_request_url = $api_endpoint . '?q=' . $search_term . '&api_key=' . $config->get('api_key');
+
+    // Make the API request.
+    $client = \Drupal::httpClient();
+    $response = $client->get($api_request_url);
+    $response_data = $response->getBody();
+
+    // Display the API response in a status message.
+    drupal_set_message($this->t('API response: @response', ['@response' => $response_data]));
   }
+
+  // Display a status message indicating that the settings were saved.
+  drupal_set_message($this->t('API connection settings have been saved.'));
+
+  parent::submitForm($form, $form_state);
+}
+
 
   /**
  *    * Returns a search form.
