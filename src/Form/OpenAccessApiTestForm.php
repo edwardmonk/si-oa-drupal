5718b7360e47269ac1641b9bbe35b25be8dc15b5
Edward Monk <edwardmonk@users.noreply.github.com>
2023-02-16 15:43:19 -0500

    Initial Commit

--- src/Form/OpenAccessApiTestForm.php
+++ src/Form/OpenAccessApiTestForm.php
@@ -29,7 +29,7 @@ class OpenAccessApiTestForm extends FormBase {
       '#required' => TRUE,
     ];
 
-    $form['submit'] = [
+    $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Test API'),
     ];
@@ -40,37 +40,30 @@ class OpenAccessApiTestForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-public function submitSearchForm(array &$form, FormStateInterface $form_state) {
-  $client = new Client();
-  $config = $this->config('smithsonian_open_access.open_access_api_connection');
-  $base_url = $config->get('api_base_url');
-  $api_key = $config->get('api_key');
-  $search = $form_state->getValue(['search_form', 'search']);
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $client = new Client();
+    $config = \Drupal::config('smithsonian_open_access.open_access_api_connection');
+    $base_url = $config->get('api_base_url');
+    $api_key = $config->get('api_key');
+    $search = $form_state->getValue('search');
 
-  $url = $base_url . '/search?q=' . urlencode($search) . '&api_key=' . $api_key;
+    $url = $base_url . '/search?q=' . urlencode($search) . '&api_key=' . $api_key;
 
-  watchdog('smithsonian_open_access', 'API Request URL: @url', ['@url' => $url]);
+    try {
+      $response = $client->get($url);
+      $response_data = $response->getBody()->getContents();
+      $response_json = json_decode($response_data);
 
-  try {
-    $response = $client->get($url);
-    $response_data = $response->getBody()->getContents();
-    $response_json = json_decode($response_data);
+      $form_state->set('last_api_response', $response_data);
+      $form_state->setValue('response', $response_data);
 
-    watchdog('smithsonian_open_access', 'API Response: @response', ['@response' => print_r($response_json, TRUE)]);
+    } catch (\Exception $e) {
+      $response_json = ['error' => $e->getMessage()];
 
-    $form_state->set('last_api_response', $response_data);
-    $form_state->setValue('response', $response_data);
+      $form_state->set('last_api_response', json_encode($response_json));
 
-  } catch (\Exception $e) {
-    $response_json = ['error' => $e->getMessage()];
-
-    watchdog('smithsonian_open_access', 'API Error: @error', ['@error' => print_r($response_json, TRUE)]);
-
-    $form_state->set('last_api_response', json_encode($response_json));
-
-    $form_state->setValue('response', json_encode($response_json));
+      $form_state->setValue('response', json_encode($response_json));
+    }
   }
-}
-
 
 }
