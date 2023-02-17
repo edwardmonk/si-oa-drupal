9f1b67a71b46ae7408561dd4184a76138b17e80f
Edward Monk <edwardmonk@users.noreply.github.com>
2023-02-17 14:52:26 -0500

    Initial Commit

--- src/Service/OpenAccessApiService.php
+++ src/Service/OpenAccessApiService.php
@@ -40,56 +40,6 @@ class OpenAccessApiTestForm extends FormBase {
   }
 
   /**
-   * {@inheritdoc}
-   */
-  public function getFormId() {
-    return 'smithsonian_open_access_test_form';
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['results'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Results'),
-      '#rows' => 20,
-      '#description' => $this->t('The JSON API response.'),
-      '#attributes' => [
-        'class' => ['open-access-results'],
-      ],
-    ];
-
-    $form['query'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Query'),
-      '#description' => $this->t('Enter a query string to search the Smithsonian Open Access API.'),
-    ];
-
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Search'),
-      '#ajax' => [
-        'callback' => '::ajaxSubmitCallback',
-        'wrapper' => 'open-access-results-wrapper',
-        'effect' => 'fade',
-      ],
-    ];
-
-    $form['#prefix'] = '<div id="open-access-test-form-wrapper">';
-    $form['#suffix'] = '</div>';
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    // Nothing to do here.
-  }
-
-  /**
    * Ajax callback for the test form submit.
    */
   public function ajaxSubmitCallback(array &$form, FormStateInterface $form_state) {
